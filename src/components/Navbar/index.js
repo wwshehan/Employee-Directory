30e3e08e8199a8d7d7ca4911a70683213281f2a1
Warren Shehan <wwshehan@gmail.com>
2020-04-03 20:31:02 -0400

    updated search and ss

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-function Navbar () {
-    return (
-        <nav className="navbar navbar-light bg-light bg-light">
-            <span className="navbar-brand mb-0 h1">Employee Directory</span>
-        </nav>
-    )
+function Navbar() {
+  return (
+    <nav className="navbar navbar-light bg-light bg-light">
+      <span className="navbar-brand mb-0 h1">Employee Directory</span>
+      <i class="fas fa-search" aria-hidden="true"></i>
+      <input
+        class="form-control form-control-sm ml-3 w-75"
+        type="text"
+        placeholder="Search"
+        aria-label="Search"
+      ></input>
+    </nav>
+  );
 }
 
 export default Navbar;
